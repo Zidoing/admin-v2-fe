377723bacd414c41045b7e4ec1947f3f507d63da
zhoulei <403907871@qq.com>
2021-04-17 21:40:01 +0800

    - project init

---
+++ src/app.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import './index.css';
+import './index.scss';
+
+ReactDOM.render(
+    <h1>hello world</h1>,
+    document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: './src/app.jsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "app.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "env", 'react'
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+            }, {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: [
+                        "css-loader",
+                        "sass-loader"
+                    ]
+                })
+            }
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        }),
+        new ExtractTextPlugin("index.css")
+    ]
+
+}
