60e69a59f3aaf16887dbc05ee971f5daf0de0c7e
zhoulei <403907871@qq.com>
2021-04-18 16:21:07 +0800

    feat: 第五章结束开启第六章

--- src/app.jsx
+++ src/app.jsx
@@ -1,96 +1,24 @@
-import React from "react";
+import React, {Component} from "react";
 import ReactDOM from 'react-dom';
-import './index.css';
-import './index.scss';
 import 'font-awesome/css/font-awesome.min.css';
+import Home from 'page/home/index.jsx'
+import {Redirect, Route, Router} from "react-router-dom";
 
-
-class Title extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return <h1>{this.props.children}</h1>
-    }
-}
-
-class App extends React.Component {
+class App extends Component {
     render() {
         return (
-            <div className="">
-                <Title>
-                    <span>app span</span>
-                </Title>
-                <hr/>
-                <Child/>
-            </div>
-        );
-    }
-}
-
-class Child extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: "zhoulei",
-            age: 1
-        }
-        // this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick(e) {
-        this.setState({
-            age: this.state.age + 1
-        })
-
-        this.props.onBgColorChange('red')
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>父组件背景色: {this.props.bgColor}</h1>
-                <button onClick={
-                    (e) => {
-                        this.handleClick(e)
-                    }}> 按钮
-                </button>
-            </div>
-
-        )
-    }
-}
-
-class Father extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            bgColor: '#999'
-        }
-    }
-
-    onBgColorChange(color) {
-        this.setState({
-            bgColor: color
-        })
-    }
-
-    render() {
-        return (
-            <div style={{background: this.state.bgColor}}>
-                <Child bgColor={this.state.bgColor} onBgColorChange={(color) => {
-                    this.onBgColorChange(color)
-                }}/>
-            </div>
+            <Router>
+                <div>
+                    <Route exact path='/' component={Home}/>
+                    <Redirect from="*" to="/"/>
+                </div>
+            </Router>
         );
     }
 
 }
 
 ReactDOM.render(
-    <div>
-        <Father/>
-    </div>,
+    <App/>,
     document.getElementById('app')
 );
