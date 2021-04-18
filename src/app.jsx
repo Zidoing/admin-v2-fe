52e2956d872aae0eca15bf59e22d98d2e5372b9d
zhoulei <403907871@qq.com>
2021-04-18 12:48:00 +0800

    - 项目各种包安装

--- src/app.jsx
+++ src/app.jsx
@@ -4,11 +4,93 @@ import './index.css';
 import './index.scss';
 import 'font-awesome/css/font-awesome.min.css';
 
+
+class Title extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <h1>{this.props.children}</h1>
+    }
+}
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="">
+                <Title>
+                    <span>app span</span>
+                </Title>
+                <hr/>
+                <Child/>
+            </div>
+        );
+    }
+}
+
+class Child extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "zhoulei",
+            age: 1
+        }
+        // this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(e) {
+        this.setState({
+            age: this.state.age + 1
+        })
+
+        this.props.onBgColorChange('red')
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>父组件背景色: {this.props.bgColor}</h1>
+                <button onClick={
+                    (e) => {
+                        this.handleClick(e)
+                    }}> 按钮
+                </button>
+            </div>
+
+        )
+    }
+}
+
+class Father extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            bgColor: '#999'
+        }
+    }
+
+    onBgColorChange(color) {
+        this.setState({
+            bgColor: color
+        })
+    }
+
+    render() {
+        return (
+            <div style={{background: this.state.bgColor}}>
+                <Child bgColor={this.state.bgColor} onBgColorChange={(color) => {
+                    this.onBgColorChange(color)
+                }}/>
+            </div>
+        );
+    }
+
+}
+
 ReactDOM.render(
     <div>
-        <i className="fa fa-address-book"></i>
-        <h1>hello s world</h1>
-    </div>
-    ,
+        <Father/>
+    </div>,
     document.getElementById('app')
 );
